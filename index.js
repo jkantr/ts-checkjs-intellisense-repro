1676b3d486054fdba81ef1874141e3d704b51cc9
Jared Kantrowitz <jared.kantrowitz@gmail.com>
2018-12-10 11:20:10 -0500

    initial repro commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const {} = require('@node-sc2/core/constants/unit-type');
+
+// make sure to `npm install` first.. then:
+// ^ ctrl+space with cursor between the {} above
+// now comment out / delete the tsconfig.json and try it again
